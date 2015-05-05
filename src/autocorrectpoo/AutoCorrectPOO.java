cbadcc428f79eaa4d9da8801ac7dacc6575ed6c4
davidzarate <davidzarate@10.48.224.10>
2015-05-05 18:47:51 -0500

    Modify main method to display AutoCorrectMainGUI.java

--- src/autocorrectpoo/AutoCorrectPOO.java
+++ src/autocorrectpoo/AutoCorrectPOO.java
@@ -3,6 +3,12 @@ package autocorrectpoo;
 public class AutoCorrectPOO {
 
     public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new AutoCorrectMainGUI().setVisible(true);
+            }
+        });
     }
     
 }
