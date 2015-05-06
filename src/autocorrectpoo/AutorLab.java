f51c4cbbc4a22e798f5c30a45682b9a2e3309379
davidzarate <davidzarate@192.168.1.68>
2015-05-05 22:58:45 -0500

    Add object reading functionality to store in a LinkedList

--- src/autocorrectpoo/AutorLab.java
+++ src/autocorrectpoo/AutorLab.java
@@ -1,9 +1,20 @@
 package autocorrectpoo;
 
+import java.util.LinkedList;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.EOFException;
+
 public class AutorLab {
     private static AutorLab sAutorLab;
+    private LinkedList<Autor> mAutoresLinkedList;
+    private ObjectInputStream input;
     
     private AutorLab(){
+        mAutoresLinkedList = new LinkedList<>();
+        initObjectInputStream();
+        addToAutoresLinkedList();
     }
     
     public static AutorLab get(){
@@ -11,5 +22,33 @@ public class AutorLab {
             sAutorLab = new AutorLab();
         }
         return sAutorLab;
+    }
+    
+    private void initObjectInputStream(){
+        try{
+            input = new ObjectInputStream(new FileInputStream("src/res/autores.dat"));
+        }
+        catch(IOException e){
+            javax.swing.JOptionPane.showMessageDialog(null, "Error loading authors.");
+            System.exit(1);
+        }
+        finally{
+        }
+    }
+    
+    private void addToAutoresLinkedList(){
+        if(input!=null){
+            try{
+                while(true){
+                    mAutoresLinkedList.add((Autor)input.readObject());
+                }
+            }
+            catch(EOFException e){
+            }
+            catch(IOException|ClassNotFoundException e){
+            }
+            finally{
+            }
+        }
     }
 }
