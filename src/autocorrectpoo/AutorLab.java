28064058f601f1d52c0c8c509b2c27b76f307801
davidzarate <davidzarate@192.168.1.68>
2015-05-05 23:45:54 -0500

    Modify object reading functionality and add modify authors functionality

--- src/autocorrectpoo/AutorLab.java
+++ src/autocorrectpoo/AutorLab.java
@@ -1,7 +1,9 @@
 package autocorrectpoo;
 
 import java.util.LinkedList;
+import java.io.ObjectOutputStream;
 import java.io.ObjectInputStream;
+import java.io.FileOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.EOFException;
@@ -10,10 +12,10 @@ public class AutorLab {
     private static AutorLab sAutorLab;
     private LinkedList<Autor> mAutoresLinkedList;
     private ObjectInputStream input;
+    private ObjectOutputStream output;
     
     private AutorLab(){
         mAutoresLinkedList = new LinkedList<>();
-        initObjectInputStream();
         addToAutoresLinkedList();
     }
     
@@ -36,7 +38,47 @@ public class AutorLab {
         }
     }
     
+    private void initObjectOutputStream(){
+        try{
+            output = new ObjectOutputStream(new FileOutputStream("src/res/autores.dat"));
+        }
+        catch(IOException e){
+            javax.swing.JOptionPane.showMessageDialog(null, "Error uploading authors.");
+            System.exit(1);
+        }
+        finally{
+        }
+    }
+    
+    private void closeFile(ObjectInputStream i){
+        try{
+            if(i != null){
+                i.close();
+            }
+        }
+        catch(IOException e){
+            javax.swing.JOptionPane.showMessageDialog(null, "Error");
+            System.exit(1);
+        }
+        finally{
+        }
+    }
+    private void closeFile(ObjectOutputStream o){
+        try{
+            if(o != null){
+                o.close();
+            }
+        }
+        catch(IOException e){
+            javax.swing.JOptionPane.showMessageDialog(null, "Error");
+            System.exit(1);
+        }
+        finally{
+        }
+    }
+    
     private void addToAutoresLinkedList(){
+        initObjectInputStream();
         if(input!=null){
             try{
                 while(true){
@@ -48,6 +90,32 @@ public class AutorLab {
             catch(IOException|ClassNotFoundException e){
             }
             finally{
+                closeFile(input);
+            }
+        }
+    }
+    
+    public void modificarAutoresDeArchivo(Autor[] nuevosAutores){
+        if(nuevosAutores != null){
+            initObjectOutputStream();
+            if(output != null){
+                try{
+                    LinkedList<Autor> nuevaLinkedList = new LinkedList<>();
+
+                    for(Autor a: nuevosAutores){
+                        nuevaLinkedList.add(a);
+                        output.writeObject(a);
+                    }
+
+                    mAutoresLinkedList = nuevaLinkedList;
+                }
+                catch(IOException e){
+                    javax.swing.JOptionPane.showMessageDialog(null, "Error saving authors");
+                    System.exit(1);
+                }
+                finally{
+                    closeFile(output);
+                }
             }
         }
     }
