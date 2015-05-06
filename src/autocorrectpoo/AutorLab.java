3745947d976c6645233d32cd800008486c2e561c
davidzarate <davidzarate@192.168.1.68>
2015-05-05 20:15:52 -0500

    Add code to function as a singleton AutorLab : singleton

--- src/autocorrectpoo/AutorLab.java
+++ src/autocorrectpoo/AutorLab.java
@@ -1,5 +1,15 @@
 package autocorrectpoo;
 
 public class AutorLab {
+    private static AutorLab sAutorLab;
     
+    private AutorLab(){
+    }
+    
+    public static AutorLab get(){
+        if(sAutorLab == null){
+            sAutorLab = new AutorLab();
+        }
+        return sAutorLab;
+    }
 }
