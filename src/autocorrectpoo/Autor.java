2c81ae3b6cf51ee27a12db90944845b11dd11423
davidzarate <davidzarate@169.254.184.79>
2015-05-07 16:15:40 -0500

    Add contructor that initializes the name of the Author and add private attribute of the name

--- src/autocorrectpoo/Autor.java
+++ src/autocorrectpoo/Autor.java
@@ -1,5 +1,10 @@
 package autocorrectpoo;
 
 public class Autor {
-    
+    private String nombre;
+    public Autor(){
+    }
+    public Autor(String nombre){
+        this.nombre = nombre;
+    }
 }
