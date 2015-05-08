bde2b8c5137da3257fc9ae2938436ebec877f8de
davidzarate <davidzarate@192.168.1.68>
2015-05-08 03:20:53 -0500

    Add text checking functionality

--- src/autocorrectpoo/Corrector.java
+++ src/autocorrectpoo/Corrector.java
@@ -1,8 +1,49 @@
 package autocorrectpoo;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class Corrector {
     public static String corregirTexto(String texto){
-        return new String();
+        try{
+            String propuesta = "";
+            String dictionary = "";
+            
+            Scanner sc = new Scanner(new File("src/res/diccionario.dat"));
+            
+            while(sc.hasNext()){
+                String currentWordWithMathRating = sc.nextLine();
+                dictionary += currentWordWithMathRating + "\n";
+            }
+            
+            sc.close();
+            sc = new Scanner(texto);
+            
+            while(sc.hasNext()){
+                String currentWord = sc.next();
+                
+                if(!searchInDictionary(dictionary, currentWord)){
+                    String[] proposals = proposal(dictionary, currentWord);
+                    propuesta += currentWord + ": ";
+                    for(int i = 0; i < proposals.length; i++){
+                        propuesta += proposals[i] + ", ";
+                    }
+                    propuesta += "\n";
+                }
+            }
+            
+            return propuesta;
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+        finally{
+            
+        }
+        
+        return null;
     }
     
     private static String obtenerMathRating(String word){
@@ -54,5 +95,40 @@ public class Corrector {
         }
         
         return currentWordSoundexNoRepeat;
+    }
+    
+    private static boolean searchInDictionary(String dictionary, String word){
+        Scanner scDictionary = new Scanner(dictionary);
+        while(scDictionary.hasNext()){
+            String dictionaryCurrentWord = scDictionary.next();
+            String dictionaryCurrentMathRating = scDictionary.next();
+            
+            word = word.toLowerCase();
+            if(word.equals(dictionaryCurrentWord)){
+                scDictionary.close();
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private static String[] proposal(String dictionary, String word){
+        Scanner scDictionary = new Scanner(dictionary);
+        String wordMathRating = obtenerMathRating(word);
+        ArrayList<String> proposals = new ArrayList<>(0);
+        while(scDictionary.hasNext()){
+            String dictionaryCurrentWord = scDictionary.next();
+            String dictionaryCurrentMathRating = scDictionary.next();
+            
+            if(wordMathRating.equals(dictionaryCurrentMathRating)){
+                proposals.add(dictionaryCurrentWord);
+            }
+        }
+        String[] value = new String[proposals.size()];
+        for(int i = 0; i < value.length; i++){
+            value[i] = proposals.get(i);
+        }
+        
+        return value;
     }
 }
